1e44ce1692dcdc488331f3168c4463eed91edc2b
Eeeeelias <Elias.Albrecht@campus.lmu.de>
2023-08-09 11:59:18 +0200

    updates to get the original annotated PPI graph

--- digger_extension/nease_annotated_ppi.py
+++ digger_extension/nease_annotated_ppi.py
@@ -10,37 +10,35 @@ ppis = [(i, j) if i < j else (j, i) for i, j in ppi_graph.edges]
 
 
 def extract_pdb_supported_ppis():
-    pdb_info = pd.read_csv("D:/Downloads/PPI_table_proteome_95_seq_id.tsv", sep="\t", low_memory=False)
-    mart_info = pd.read_csv("sourcedata/mart_export.txt", sep="\t")
+    pdb_info = pd.read_csv("../data_nease/PPI_table_clean.tsv", sep="\t")
+    df = pd.read_csv("../data_nease/HUMAN_9606_idmapping_GeneIDs.dat", sep="\t", header=None)
+    df.columns = ['UniProtID', "GeneID", "EntrezID"]
+    id_mapping = df.set_index('UniProtID')['EntrezID'].to_dict()
+    # mart_info = pd.read_csv("../sourcedata/mart_export.txt", sep="\t")
+    # id_mapping = mart_info.set_index('UniProtKB/Swiss-Prot ID')['NCBI gene (formerly Entrezgene) ID'].to_dict()
 
-    uniprot_entrez = dict()
-    for idx, row in mart_info.iterrows():
-        if pd.isna(row['NCBI gene (formerly Entrezgene) ID']):
-            continue
-        uniprot_entrez[row['UniProtKB/Swiss-Prot ID']] = row['NCBI gene (formerly Entrezgene) ID']
-
-    print("Proteins mapped to entrez IDs:", len(uniprot_entrez))
-
+    print("Proteins mapped to entrez IDs:", len(id_mapping))
+    orig_interactions = set()
     interactions = set()
-    pdb_len = len(pdb_info)
+    key_errors = 0
     for idx, row in pdb_info.iterrows():
-        # just for progress
-        if idx % 100_000 == 0:
-            print(round(idx / pdb_len, 2) * 100, "%")
         try:
+            orig_interactions.add((row['u_ac_1'], row['u_ac_2']))
             # get entrez id of uniprot protein
-            gene1 = str(int(uniprot_entrez.get(row['u_ac_1'])))
-            gene2 = str(int(uniprot_entrez.get(row['u_ac_2'])))
+            gene1 = str(id_mapping.get(row['u_ac_1']))
+            gene2 = str(id_mapping.get(row['u_ac_2']))
         except KeyError:
+            key_errors += 1
             continue
-        except TypeError:
+        if gene1 == 'None' or gene2 == 'None':
             continue
         # add it in sorted order, so it's easier to compare later
-        interactions.add((gene1, gene2)) if gene1 < gene2 else interactions.add((gene2, gene1))
+        interactions.add((gene1, gene2)) # if gene1 < gene2 else interactions.add((gene2, gene1))
 
+    print("Key errors:", key_errors)
     # write it to a file
     print("PDB interactions:", len(interactions))
-    with open("residue_supported_interactions.tsv", 'w') as f:
+    with open("../residue_supported_interactions.tsv", 'w') as f:
         for i, j in interactions:
             f.write(f"{i}\t{j}\n")
 
@@ -74,7 +72,10 @@ def filter_by_pdb(pdb_path):
         for line in f:
             split = line.strip().split("\t")
             pdb_db.add((split[0], split[1]))
-    filtered = set(ppis) & pdb_db
+    pdb_db_reversed = pdb_db
+    for i, j in pdb_db:
+        pdb_db_reversed.add((j, i))
+    filtered = set(ppis) & pdb_db_reversed
     print(f"{len(filtered):,} interactions supported by PDB")
     return list(filtered)
 
@@ -92,13 +93,34 @@ def pathway_degree(pathways: pd.DataFrame, ppi_ddi_graph: nx.Graph):
     return new_degrees
 
 
+def combine_exon_residue_interactions(exon_level: str, residue_level: str):
+    exon_interactions = set()
+    residue_interactions = set()
+
+    with open(exon_level, 'r') as f:
+        for line in f.readlines():
+            el1 = line.split("\t")[0]
+            el2 = line.split("\t")[1].strip()
+            exon_interactions.add((el1, el2))
+    with open(residue_level, 'r') as f:
+        for line in f.readlines():
+            el1 = line.split("\t")[0]
+            el2 = line.split("\t")[1].strip()
+            residue_interactions.add((el1, el2))
+
+    print("Exon level interactions:", len(exon_interactions))
+    print("Residue level interactions:", len(residue_interactions))
+    print("Combined:", len(exon_interactions | residue_interactions))
+
+
 if __name__ == '__main__':
+    extract_pdb_supported_ppis()
     ppis_filtered = filter_by_ddi(path + "/network/graph_human_old.pkl")
     ppis_filtered += filter_by_elm(path + "/database/ELM_interactions")
-    ppis_filtered += filter_by_pdb("../residue_supported_interactions_exons.tsv")
+    ppis_filtered += filter_by_pdb("../residue_supported_interactions.tsv")
     pathways = pickle.load(open(path + "/pathways/pathways_human", 'rb'))
     filtered_graph = nx.Graph(ppis_filtered)
-    print(filtered_graph)
+    print("PPI graph supported by DDI/ELM/PDB:", filtered_graph)
     # new_degrees = pathway_degree(pathways, filtered_graph)
     # # pathways['Degree in the PPI/DDI_old'] = pathways['Degree in the PPI/DDI']
     # # pathways['Degree in the structural PPI_old'] = pathways['Degree in the structural PPI']
